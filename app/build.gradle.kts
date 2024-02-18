7f30f6fa3ecce90c1d443550ec4ebba992cb16b1
v.podkopaev <max.astin@yandex.ru>
2024-02-18 22:04:16 +0200

    update min sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.bunbeauty.fakelivestream"
-        minSdk = 24
+        minSdk = 27
         targetSdk = 34
         versionCode = 100
         versionName = "1.0.0"
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
