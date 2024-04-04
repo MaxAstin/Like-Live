3fc3b7d9b11d0f0d21048b15db3eb4eb923e9b11
v.podkopaev <max.astin@yandex.ru>
2024-04-04 22:56:43 +0300

    update version up to 1.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bunbeauty.fakelivestream"
         minSdk = 27
         targetSdk = 34
-        versionCode = 141
-        versionName = "1.4.1"
+        versionCode = 152
+        versionName = "1.5.2"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
